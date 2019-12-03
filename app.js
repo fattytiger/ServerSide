5de58c17708dc7fabc01bbdc968080586d512cc7
fatty <315782792@qq.com>
2019-12-03 23:05:31 +0800

    add the koa fromwork & router plugin

---
+++ app.js
@@ -0,0 +1,23 @@
+const Koa  = require("koa")
+
+const Router = require("koa-router")
+
+const app  = new Koa()
+
+const router = new Router()
+
+// app.use( async ctx => ctx.body = 'hello koa')
+
+//router test
+router.get('/' ,(ctx) => ctx.body = 'Im /')
+
+router.get('/index',(ctx) => ctx.body = 'Im index')
+
+router.get('/news',(ctx) => ctx.body = 'Im news')
+
+
+
+//when you want to use the router.routes,you need to regist the router first
+app.use(router.routes())
+
+app.listen(3000)
