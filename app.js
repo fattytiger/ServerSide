28b295a60f5cf852de1aea112274141ca914d8e2
NickyLi <376514069@qq.com>
2019-12-10 22:36:31 +0800

    change the render engine from the ejs to koa-art-template

--- app.js
+++ app.js
@@ -6,19 +6,27 @@ const app = new Koa()
 
 const router = new Router()
 
-const render = require('koa-ejs')
+// const render = require('koa-ejs')
+const render = require('koa-art-template')
 
 const path = require('path')
 
 const server = require('koa-static')
 
 
+
+// render(app,{
+//     root:path.join(__dirname,`view/master-page`),
+//     layout: `master`,
+//     viewExt:`html`,
+//     cache:false,
+//     debug:false
+// })
+
 render(app,{
-    root:path.join(__dirname,`view/master-page`),
-    layout: `master`,
-    viewExt:`html`,
-    cache:false,
-    debug:false
+    root: path.join(__dirname, 'view/master-page'),
+  extname: '.html',
+  debug: process.env.NODE_ENV !== 'production'
 })
 
 app.use(server(__dirname+`/node_modules/bootstrap`))
@@ -27,7 +35,21 @@ app.use(server(__dirname+`/node_modules/popper.js`))
 
 //router test
 router.all('/', async ctx => {
-    await ctx.render('../pages/index')
+
+    let list = [
+        1,2,3,4,5
+    ] 
+    let  obj= { 
+        name:`zhangsan` 
+    } 
+    let val = `<h1>12<h1>` 
+    await ctx.render('../master-page/master',{ 
+
+        list:list, 
+        obj,
+        val
+
+    })
 })
     .all('/product', async ctx => {
     await ctx.render('../pages/product')
